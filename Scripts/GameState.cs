d371b3c1c08bf062c84a3463320cff93c7ca93da
nevo <arceuid727@gmail.com>
2024-01-08 05:41:59 +0800

    Added Locked door and GameState singleton  but not implemented logic yet

---
+++ Scripts/GameState.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace saphireStreaks.Scripts
+{
+    public static class GameState
+    {
+        // Singleton for Game states like keycard and which doors are opened
+    }
+}
