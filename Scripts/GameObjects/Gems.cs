741d2761c6f6f40fd9b900e246d23d355fb3049f
nevo <arceuid727@gmail.com>
2024-01-08 05:18:02 +0800

    Revert "Added gem logic"

--- Scripts/GameObjects/Gems.cs
+++ Scripts/GameObjects/Gems.cs
@@ -3,7 +3,7 @@ using System;
 
 public class Gems : Node2D
 {
-	[Export] public Color ColorType;
+	[Export] public Color Color;
 	public override void _Ready()
 	{
 		
@@ -17,6 +17,6 @@ public class Gems : Node2D
 
 	public enum Color 
 	{
-		Blue, Yellow, Red, Purple
+		Blue, Yellow, Red Purple
 	}
 }
