9983b0c50e20f7fe48657c5035beaf6000138133
nevo <arceuid727@gmail.com>
2024-01-09 17:01:15 +0800

    Made Lever Sprite Worked

--- Scripts/GameObjects/Lever.cs
+++ Scripts/GameObjects/Lever.cs
@@ -5,15 +5,25 @@ public class Lever : Node2D
 {
     [Export] public bool LeverState { get => _leverState; set { _leverState = value; OnLeverStateChanged(); } }
     private bool _leverState;
+
+    private int _currentFrame;
+    private Sprite LeverSprite;
+
     public event Action LeverStateChangedEvent;
     private Interactable Interactable;
     private void OnLeverStateChanged()
     {
+        _currentFrame = Mathf.Wrap(_currentFrame + 1, 0, LeverSprite.Hframes - 1);
+        this.LeverSprite.Frame = _currentFrame;
+
         this.LeverStateChangedEvent?.Invoke();
     }
 
     public override void _Ready()
     {
+        this.LeverSprite = base.GetNode<Sprite>("Sprite");
+
         this.Interactable = base.GetNode<Interactable>("Interactable");
+        this.Interactable.Interacted += OnLeverStateChanged;
     }
 }
