566a86d057a97903ca92374cb30a436d68ff79ef
nevo <arceuid727@gmail.com>
2024-01-09 18:44:19 +0800

    Added outline to gameobjects

--- Scripts/GameObjects/Gem.cs
+++ Scripts/GameObjects/Gem.cs
@@ -3,14 +3,19 @@ using System;
 
 public class Gem : Node2D
 {
-	[Export] public Color ColorType;
+	[Export] public Color ColorType { get; set; }
+	[Export] public bool IsInteractable { get; set; } = true;
 	private Interactable Interactable;
+	private Sprite GemSprite;
+    private ShaderMaterial ShaderMaterial = GD.Load<ShaderMaterial>("res://Scripts/Shared/OutlineShader.tres");
 	public override void _Ready()
 	{
+		this.GemSprite = base.GetNode<Sprite>("Sprite");
 		this.Interactable = this.GetNode<Interactable>("Interactable");
 
 		// Give the player gem when the player interacts with it
-		this.Interactable.Interacted += () => {
+		this.Interactable.Interacted += () =>
+		{
 			GameState.AddGem(ColorType);
 			this.Visible = false;
 		};
@@ -30,9 +35,25 @@ public class Gem : Node2D
 				this.GetNode<Sprite>("Sprite").Texture = GD.Load<Texture>("res://Assets/LevelSprites/GemPurple.png");
 				break;
 		}
+
+		this.Interactable.PlayerEntered += OnPlayerEntered;
+		this.Interactable.PlayerExited += OnPlayerExited;
 	}
 
-	public enum Color 
+	private void OnPlayerEntered()
+	{
+		if (IsInteractable)
+		{
+			this.GemSprite.Material = ShaderMaterial;
+		}
+	}
+
+	private void OnPlayerExited()
+	{
+		this.GemSprite.Material = null;
+	}
+
+	public enum Color
 	{
 		Blue, Yellow, Red, Purple
 	}
--- Scripts/GameObjects/Lever.cs
+++ Scripts/GameObjects/Lever.cs
@@ -4,13 +4,15 @@ using System;
 public class Lever : Node2D
 {
     [Export] public bool LeverState { get => _leverState; set { _leverState = value; OnLeverStateChanged(); } }
-    private bool _leverState;
+    [Export] public bool IsInteractable;
 
+    private bool _leverState;
     private int _currentFrame;
     private Sprite LeverSprite;
 
     public event Action LeverStateChangedEvent;
     private Interactable Interactable;
+    private ShaderMaterial ShaderMaterial = GD.Load<ShaderMaterial>("res://Scripts/Shared/OutlineShader.tres");
     private void OnLeverStateChanged()
     {
         _currentFrame = Mathf.Wrap(_currentFrame, 0, LeverSprite.Hframes);
@@ -26,5 +28,20 @@ public class Lever : Node2D
 
         this.Interactable = base.GetNode<Interactable>("Interactable");
         this.Interactable.Interacted += OnLeverStateChanged;
+        this.Interactable.PlayerEntered += OnPlayerEntered;
+        this.Interactable.PlayerExited += OnPlayerExited;
+    }
+
+    private void OnPlayerEntered()
+    {
+        if (IsInteractable)
+        {
+            this.LeverSprite.Material = ShaderMaterial;
+        }
+    }
+
+    private void OnPlayerExited()
+    {
+        this.LeverSprite.Material = null;
     }
 }
