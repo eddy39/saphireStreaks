b9a9e92199c14d422e90e0a496ee7addc0f39320
eddy <65038622+eddy39@users.noreply.github.com>
2024-01-12 12:40:36 +0100

    added interactable ui

--- Scripts/Shared/Interactable.cs
+++ Scripts/Shared/Interactable.cs
@@ -7,10 +7,16 @@ public class Interactable : Area2D
     public event Action PlayerExited;
     public event Action Interacted;
     public bool PlayerInArea { get; set; }
+
+    public Sprite KeyBG;
     public override void _Ready()
     {
         Connect("body_entered", this, nameof(OnPlayerEntered));
         Connect("body_exited", this, nameof(OnPlayerExited));
+
+        KeyBG = GetNode<Sprite>("KeyBG");
+        KeyBG.Visible = false;
+        KeyBG.GlobalRotation = 0;
     }
     
     private void OnPlayerEntered(Node2D body)
@@ -20,6 +26,8 @@ public class Interactable : Area2D
 
         PlayerInArea = true;
         PlayerEntered?.Invoke();
+
+        KeyBG.Visible = true;
     }
 
     private void OnPlayerExited(Node2D body)
@@ -29,6 +37,8 @@ public class Interactable : Area2D
 
         PlayerInArea = false;
         PlayerExited?.Invoke();
+    
+        KeyBG.Visible = false;
     }
 
     public override void _Input(InputEvent @event)
